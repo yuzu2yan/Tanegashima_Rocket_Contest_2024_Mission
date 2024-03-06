a09b61ea696da582c9e255e775f2e259220c4ad0
yuzu <kzjoe222@gmail.com>
2024-03-06 12:25:55 +0900

    modify

--- arm/src/ros_arm/chatter/chat.py
+++ arm/src/ros_arm/chatter/chat.py
@@ -2,37 +2,34 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
 
-class Talker(Node):
+class SubscriberNode(Node):
 
     def __init__(self):
-        super().__init__('arm_talker')
-        self.publisher_ = self.create_publisher(Int32, 'chatter', 10)
-        self.timer_ = self.create_timer(0.5, self.timer_callback)
-        self.msg_ = Int32
-        
-    def timer_callback(self):
+        super().__init__('subscriber_node')
+        self.subscription = self.create_subscription(Int32, 'number', self.number_callback, 10)
+        self.subscription  # subscriptionを破棄しないようにするために変数に割り当てる
+        self.publisher_ = self.create_publisher(Int32, 'number', 10)
+        self.stop_publishing = False  # パブリッシュ停止フラグ
+
+    def number_callback(self, msg):
+        if msg.data == 1:
+            self.stop_publishing = True
+
+    def publish_number(self):
         msg = Int32()
         msg.data = 2
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
 
-class Listener(Node):
-
-    def __init__(self):
-        super().__init__('arm_listener')
-        self.subscription_ = self.create_subscription(
-            Int32, 'chatter', self.listener_callback, 10)
-        self.subscription_
-
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
-
-
-if __name__ == '__main__':
-    rclpy.init()
-    listener = Listener()
-    rclpy.spin(listener)
-    talker = Talker()
-    rclpy.spin(talker)
+def main(args=None):
+    rclpy.init(args=args)
+    node = SubscriberNode()
+    timer = node.create_timer(1.0, node.publish_number)  # 1秒ごとに数値2をパブリッシュ
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if node.stop_publishing:
+            timer.cancel()
+    node.destroy_node()
     rclpy.shutdown()
 
+if __name__ == '__main__':
+    main()
--- truck/src/ros_truck/chatter/chat.py
+++ truck/src/ros_truck/chatter/chat.py
@@ -2,38 +2,24 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
 
-class Talker(Node):
+class PublisherNode(Node):
 
     def __init__(self):
-        super().__init__('truck_talker')
-        self.publisher_ = self.create_publisher(Int32, 'chatter', 10)
-        self.timer_ = self.create_timer(0.5, self.timer_callback)
-        self.msg_ = Int32
-        
-    def timer_callback(self):
+        super().__init__('publisher_node')
+        self.publisher_ = self.create_publisher(Int32, 'number', 10)
+        self.timer = self.create_timer(1.0, self.publish_number)  # 1秒ごとに数値1をパブリッシュ
+
+    def publish_number(self):
         msg = Int32()
         msg.data = 1
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
 
-class Listener(Node):
-
-    def __init__(self):
-        super().__init__('truck_listener')
-        self.subscription_ = self.create_subscription(
-            Int32, 'chatter', self.listener_callback, 10)
-        self.subscription_
-
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
-
-
+def main(args=None):
+    rclpy.init(args=args)
+    node = PublisherNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
-    rclpy.init()
-    talker = Talker()
-    rclpy.spin(talker)
-    listener = Listener()
-    rclpy.spin(listener)
-    talker.destroy_node()
-    rclpy.shutdown()
+    main()
